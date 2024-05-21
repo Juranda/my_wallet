e467911f7f78c9fdd2498a575ff58a5484ae96cc
Gabriel R <gabrielrvieira2019@gmail.com>
2024-05-21 16:59:32 -0300

    professor entrar trilhao nao mais problema

--- lib/pages/trilhas/trails_view.dart
+++ lib/pages/trilhas/trails_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_wallet/pages/account_settings/models/professor.dart';
 import 'package:my_wallet/pages/account_settings/models/role.dart';
 import 'package:my_wallet/user_provider.dart';
 import 'package:provider/provider.dart';
@@ -16,10 +17,16 @@ class _TrailsViewState extends State<TrailsView> {
     if (_user_provider.role == Role.professor) {
       return await Supabase.instance.client.from('trilha').select();
     } else {
-      return await Supabase.instance.client
+      final response = await Supabase.instance.client
           .from('trilha_turma')
-          .select()
+          .select('id_trilha')
           .eq('id_turma', _user_provider.id_turma);
+      final List<int> ids = response.map((e) => e['id_trilha'] as int).toList();
+      final newResponse = await Supabase.instance.client
+          .from('trilha')
+          .select()
+          .inFilter('id', ids);
+      return newResponse;
     }
   }
 
@@ -31,11 +38,45 @@ class _TrailsViewState extends State<TrailsView> {
     _user_provider = Provider.of<UserProvider>(context, listen: false);
   }
 
+  Future<bool> trilhaJaLiberada(int trilhaID) async {
+    final response = await Supabase.instance.client
+        .from('trilha_turma')
+        .select()
+        .eq('id_trilha', trilhaID)
+        .eq('id_turma', _user_provider.professor!.id_turma);
+
+    return response.isNotEmpty;
+  }
+
   Future<void> liberarTrilha(int trilhaID) async {
+    if (await trilhaJaLiberada(trilhaID)) return;
+
     await Supabase.instance.client.from('trilha_turma').insert({
       'id_turma': _user_provider.professor!.id_turma,
       'id_trilha': trilhaID
     });
+
+    //pra cada aluno da turma, criar a relação atividade-aluno de todas as atividades dessa trilha
+    final alunos = await Supabase.instance.client
+        .from('aluno')
+        .select()
+        .eq('id_turma', _user_provider.professor!.id_turma);
+    final atividades = await Supabase.instance.client
+        .from('atividade')
+        .select()
+        .eq('id_trilha', trilhaID);
+    for (Map<String, dynamic> aluno in alunos) {
+      for (Map<String, dynamic> atividade in atividades) {
+        await Supabase.instance.client.from('aluno_atividade').insert({
+          'liberada': atividades[0] == atividade,
+          'completada': false,
+          'id_aluno': aluno['id'],
+          'id_atividade': atividade['id']
+        });
+      }
+    }
+
+    setState(() {});
   }
 
   @override
@@ -62,7 +103,9 @@ class _TrailsViewState extends State<TrailsView> {
           height: MediaQuery.of(context).size.height - 244,
           width: MediaQuery.of(context).size.width,
           child: FutureBuilder(
-            future: Supabase.instance.client.from('trilha').select('*'),
+            //se é professor, mostre todas as trilhas
+            //se for aluno, mostre só as da turma (fetch trilhas)
+            future: fetchTrilhas(),
             builder: (context, snapshot) {
               if (!snapshot.hasData) {
                 return CircularProgressIndicator();
@@ -74,7 +117,8 @@ class _TrailsViewState extends State<TrailsView> {
                   itemBuilder: (context, index) => TrailItem(
                       Trail(trilhas[index]['id'], trilhas[index]['nome'],
                           trilhas[index]['descricao'], false),
-                      liberarTrilha),
+                      liberarTrilha,
+                      trilhaJaLiberada),
                 );
               }
             },
@@ -85,18 +129,34 @@ class _TrailsViewState extends State<TrailsView> {
   }
 }
 
-class TrailItem extends StatelessWidget {
+class TrailItem extends StatefulWidget {
   final Trail trail;
   final Future<void> Function(int) liberarTrilha;
-  const TrailItem(this.trail, this.liberarTrilha, {super.key});
+  final Future<bool> Function(int) trilhaJaLiberada;
+  const TrailItem(this.trail, this.liberarTrilha, this.trilhaJaLiberada,
+      {super.key});
+
+  @override
+  State<TrailItem> createState() => _TrailItemState();
+}
+
+class _TrailItemState extends State<TrailItem> {
+  late UserProvider _user_provider;
+
+  void abrirTrilha(context) async {
+    Navigator.pushNamed(context, '/home/trails/trail', arguments: widget.trail);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _user_provider = Provider.of<UserProvider>(context, listen: false);
+  }
 
   @override
   Widget build(BuildContext context) {
     Color color = Theme.of(context).colorScheme.secondary;
-    UserProvider _user_provider =
-        Provider.of<UserProvider>(context, listen: true);
-
-    if (trail.completed) {
+    if (widget.trail.completed) {
       color = color.withOpacity(0.5);
     }
 
@@ -105,10 +165,7 @@ class TrailItem extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: InkWell(
-          onTap: () {
-            Navigator.pushNamed(context, '/home/trails/trail',
-                arguments: trail);
-          },
+          onTap: () => abrirTrilha(context),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
@@ -126,12 +183,12 @@ class TrailItem extends StatelessWidget {
                   child: Column(
                     children: [
                       Text(
-                        trail.name,
+                        widget.trail.name,
                         style: Theme.of(context).textTheme.headlineMedium,
                         textAlign: TextAlign.left,
                       ),
                       Text(
-                        trail.description,
+                        widget.trail.description,
                         textAlign: TextAlign.left,
                         softWrap: true,
                         overflow: TextOverflow.ellipsis,
@@ -142,13 +199,44 @@ class TrailItem extends StatelessWidget {
               ),
               if (_user_provider.role == Role.professor)
                 IconButton(
-                  onPressed: () {
-                    liberarTrilha(trail.id);
-                  },
-                  icon: Icon(
-                    trail.completed ? Icons.check_rounded : Icons.lock,
-                  ),
-                )
+                    onPressed: () async {
+                      if (await widget.trilhaJaLiberada(widget.trail.id)) {
+                        showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                                  content: Text('A trilha já foi liberada!'),
+                                  actions: [
+                                    ElevatedButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: Text('Ok'),
+                                    )
+                                  ],
+                                ));
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            content: Text('Trilha liberada!'),
+                            actions: [
+                              ElevatedButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: Text('Ok'),
+                              )
+                            ],
+                          ),
+                        );
+                        widget.liberarTrilha(widget.trail.id);
+                      }
+                    },
+                    icon: FutureBuilder<bool>(
+                        future: widget.trilhaJaLiberada(widget.trail.id),
+                        builder: (context, snapshot) {
+                          if (snapshot.data == true) {
+                            return Icon(Icons.check);
+                          } else {
+                            return Icon(Icons.lock);
+                          }
+                        }))
             ],
           ),
         ),
@@ -166,20 +254,91 @@ class Trail {
   Trail(this.id, this.name, this.description, this.completed);
 }
 
-class TrailView extends StatelessWidget {
+class TrailView extends StatefulWidget {
   const TrailView({super.key});
 
   @override
+  State<TrailView> createState() => _TrailViewState();
+}
+
+class _TrailViewState extends State<TrailView> {
+  @override
   Widget build(BuildContext context) {
     Trail trail = ModalRoute.of(context)!.settings.arguments as Trail;
+    UserProvider _user_provider =
+        Provider.of<UserProvider>(context, listen: true);
+
+    Future<List<Map<String, dynamic>>> fetchAtividades() async {
+      if (_user_provider.role == Role.professor) return [];
+
+      //pega o id de todas as atividades de aluno_atividade desse aluno
+      final response = await Supabase.instance.client
+          .from('aluno_atividade')
+          .select('id_atividade')
+          .eq('id_aluno', _user_provider.aluno!.id);
+
+      //transforma todos esses ids em uma lista
+      final List<int> ids =
+          response.map((e) => e['id_atividade'] as int).toList();
+
+      //retorna todas as atividades que tem um desses ids e que fazem parte da trilha atual
+      return await Supabase.instance.client
+          .from('atividade')
+          .select()
+          .eq('id_trilha', trail.id)
+          .inFilter('id', ids);
+    }
+
+    Future<void> completarAtividade(int atividadeID,
+        {bool completarTudo = false}) async {
+      if (completarTudo) {
+        await Supabase.instance.client.from('aluno_atividade').update(
+            {'completada': true}).eq('id_aluno', _user_provider.aluno!.id);
+      } else {
+        await Supabase.instance.client
+            .from('aluno_atividade')
+            .update({'completada': true})
+            .eq('id_aluno', _user_provider.aluno!.id)
+            .eq('id_atividade', atividadeID);
+      }
+      setState(() {});
+    }
 
     return Scaffold(
       body: Column(
+        mainAxisSize: MainAxisSize.max,
         children: [
           Text(
             trail.name,
             style: Theme.of(context).textTheme.bodyLarge,
-          )
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height - 244,
+            width: MediaQuery.of(context).size.width,
+            child: FutureBuilder(
+                future: fetchAtividades(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return CircularProgressIndicator();
+                  } else {
+                    final atividades = snapshot.data!;
+                    return ListView.builder(
+                        itemCount: atividades.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(atividades[index]['descricao']),
+                            trailing: Text(atividades[index]['id'].toString()),
+                          );
+                        });
+                  }
+                }),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                //só para testes
+                completarAtividade(1, completarTudo: true);
+              },
+              child: Text('completar todas as atividades!!! (muito facil)'))
         ],
       ),
     );
