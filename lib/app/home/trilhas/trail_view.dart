0aa9e0e197b09063347fa6afcd1512e647a17ee4
Felipe <felipeonlinebr@gmail.com>
2024-09-23 19:30:55 -0300

    criacao inicial do trail view

--- lib/app/home/trilhas/trail_view.dart
+++ lib/app/home/trilhas/trail_view.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:my_wallet/app/models/trail.dart';
+import 'package:my_wallet/services/mywallet.dart';
 
-class TrailView extends StatelessWidget {
-  late Trail trail;
-
+class TrailView extends StatefulWidget {
   TrailView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    this.trail = ModalRoute.of(context)!.settings.arguments as Trail;
+  State<TrailView> createState() => _TrailViewState();
+}
 
-    return Container();
+class _TrailViewState extends State<TrailView> {
+  late Trilha trilha;
+
+  @override
+  Widget build(BuildContext context) {
+    this.trilha = ModalRoute.of(context)!.settings.arguments as Trilha;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${trilha.nome} - ${trilha.escolaridade.name}"),
+      ),
+      body: FutureBuilder(
+        future: MyWallet.trailsService.getAllTrilhasDoAluno(1, 1),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return AlertDialog(
+              title: const Text('Erro'),
+              content: Text(snapshot.error.toString()),
+            );
+          }
+
+          if (snapshot.hasData == false) {
+            return Expanded(
+              child: Center(
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+              ),
+            );
+          }
+
+          return Column(
+            children: snapshot.data![0].atividades.map((e) {
+              return InkWell(
+                onTap: () => print('Hello'),
+                child: Container(
+                  child: Text(e.atividade.enunciado),
+                ),
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
   }
 }
